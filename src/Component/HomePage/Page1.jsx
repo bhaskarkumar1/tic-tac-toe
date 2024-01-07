3d5ea7f78dda05f860c7e173ea1b95266c376911
Bhaskar <bhaskarkumar.india@gmail.com>
2024-01-07 18:38:20 +0530

    toast-notification added

--- src/Component/HomePage/Page1.jsx
+++ src/Component/HomePage/Page1.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 import "./Page1.css";
 
+
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 function Page1() {
+  // const notify = () =>
+  //   toast("Invite link copied", {
+  //     className: "custom-toast",
+  //     toastClassName: "custom-toast-text",
+  //   });
+
+
+    const notify = async () => {
+      try {
+        // Your logic to generate the invite link
+        const inviteLink = "https://www.google.comcc";
+  
+        // Copy the link to the clipboard
+        await navigator.clipboard.writeText(inviteLink);
+  
+        // Show a toast notification
+        // toast.success("Invite link copied!");
+        toast("Invite link copied", {
+          className: "custom-toast",
+          toastClassName: "custom-toast-text",
+        })
+
+      } catch (error) {
+        console.error("Error copying invite link:", error);
+        // Show an error toast notification
+        toast.error("Error copying invite link");
+      }
+    };
+    
+
+
   return (
     <div className="page1">
       <div className="inner-pg-1">
@@ -33,7 +71,14 @@ function Page1() {
           NEW GAME ( VS HUMAN ) Coming soon
         </button>
 
-        <button className="btn-3">Invite Your Friend </button>
+        <button className="btn-3" onClick={notify}>Invite Your Friend </button>
+        <ToastContainer  
+                  theme= "dark"
+
+                  style={{ width: "400px", textAlign: "center"}} // Apply your custom styles here
+
+        />
+
       </div>
       </div>
 
